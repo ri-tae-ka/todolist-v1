362b30bbc453019be5cc470c3b00962992987d08
ri-tae-ka <ritzthareja@gmail.com>
2021-07-30 00:47:26 +0530

    weekday name ejs template using switch case

--- app.js
+++ app.js
@@ -13,13 +13,35 @@ app.get("/", function(req, res) {
     var currentDay = today.getDay();
     var day = " ";
 
-    if (currentDay === 6 || currentDay === 0) {
-        day = "Weekend";
-    } else {
-        day = "Weekday";
+    switch(currentDay) {
+        case 0 : day = "Sunday";
+            break;
+        
+        case 1 : day = "Monday";
+            break;
+        
+        case 2 : day = "Tuesday";
+            break;
+        
+        case 3 : day = "Wednesday";
+            break;
+        
+        case 4 : day = "Thursday";
+            break;
+        
+        case 5 : day = "Friday";
+            break;
+        
+        case 6 : day = "Saturday";
+            break;
+        
+        default : console.log("Error! Current day is equal to " + currentDay);
+        
     }
 
-    res.render("list", {kindOfDay: day});
+    res.render("list", {
+        kindOfDay: day
+    });
 
 });
 
