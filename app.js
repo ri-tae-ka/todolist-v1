59c09f9c6aec1ceb44c73fbcc9bdd718e508cf03
ri-tae-ka <ritzthareja@gmail.com>
2021-07-30 10:34:50 +0530

    toLocaleString function to display day

--- app.js
+++ app.js
@@ -10,41 +10,20 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res) {
     
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = " ";
+    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    switch(currentDay) {
-        case 0 : day = "Sunday";
-            break;
-        
-        case 1 : day = "Monday";
-            break;
-        
-        case 2 : day = "Tuesday";
-            break;
-        
-        case 3 : day = "Wednesday";
-            break;
-        
-        case 4 : day = "Thursday";
-            break;
-        
-        case 5 : day = "Friday";
-            break;
-        
-        case 6 : day = "Saturday";
-            break;
-        
-        default : console.log("Error! Current day is equal to " + currentDay);
-        
-    }
+    var day = today.toLocaleString("en-us", options);
 
     res.render("list", {
         kindOfDay: day
     });
 
 });
-
 
 
 app.listen(3000, function() {
